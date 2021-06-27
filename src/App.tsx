0b736b740a7d05a798c4432fbc0375a90ad2069d
Leonardo Avelino <leonardo.avelino16@hotmail.com>
2021-06-27 10:53:56 -0300

    feat(): add useMemo

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 import { Item } from './components/Item'
 
 function App() {
   const [items, setItems] = useState<string[]>([])
+  const [newItem, setNewItem] = useState('')
 
   function addItemToList() {
     setItems([...items, `Item ${items.length}`])
+
+    setNewItem('')
   }
+  
+
+  const countItemsWithOne = useMemo(() => {
+    console.log('render countItemsWithOne');
+    
+    return items.filter(item => item.includes('1')).length
+  }, [items]) 
+ 
+  /*
+  const countItemsWithOne = () => {
+    console.log('render countItemsWithOne');
+    
+    return items.filter(item => item.includes('1')).length
+  }
+  */
 
   return (
     <div>
+      <input type="text" onChange={e => setNewItem(e.target.value)} value={newItem} />
       <button onClick={addItemToList}>
         Add
       </button>
@@ -19,6 +38,9 @@ function App() {
           return <Item key={item} title={item} />
         })}
       </ul>
+      <div>
+        Contagem: {countItemsWithOne}
+      </div>
     </div>
   );
 }
--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -1,7 +1,11 @@
+import { memo } from 'react'
+
 type ItemProps = {
   title: string
 }
 
-export function Item({title}: ItemProps) {
+function ItemComponent({title}: ItemProps) {
   return <li>{title}</li>
 }
+
+export const Item = memo(ItemComponent)
